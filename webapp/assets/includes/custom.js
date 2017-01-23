8a1a3815bf902d6ecc510dd2c4006a06fcfd8259
Sahil Shridhar <sahil95@gmail.com>
2017-01-23 15:52:13 -0800

    Updated CSS

--- webapp/assets/includes/custom.js
+++ webapp/assets/includes/custom.js
@@ -1,3 +1,20 @@
-$(document).ready(function(){
-	alert("hi");
+$(document).ready(function() {
+	// setOrientationListener(); 
+	$('#calendar').calendar({
+		type: 'date'
+	});
 });
+
+// function setOrientationListener() {
+// 	rotationInterval = setInterval(function() {
+// 		updateOrientation();
+// 	}, 500);
+// }
+
+// function updateOrientation() {
+// 	if ($('body').width() < 1024) {
+// 		$('.page').removeClass('landscape').addClass('portrait');
+// 	} else {
+// 		$('.page').removeClass('portrait').addClass('landscape');
+// 	}
+// }
