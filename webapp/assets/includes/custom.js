38055eaad6006e5f18db6ab910ed218c07194b45
Lilly An <li.liwei94@gmail.com>
2017-01-23 13:22:33 -0800

    updated

---
+++ webapp/assets/includes/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+	setOrientationListener();
+});
+
+function setOrientationListener() {
+	rotationInterval = setInterval(function() {
+		updateOrientation();
+	}, 500);
+}
+
+function updateOrientation() {
+	if ($('body').width() < 1024) {
+		$('.page').removeClass('landscape').addClass('portrait');
+	} else {
+		$('.page').removeClass('portrait').addClass('landscape');
+	}
+}
