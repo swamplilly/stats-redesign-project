dd05e093596a9636aff3341f586035fc846fd3a1
Lilly An <li.liwei94@gmail.com>
2017-01-24 12:44:26 -0800

    added accordion mockup

--- webapp/assets/includes/custom.js
+++ webapp/assets/includes/custom.js
@@ -1,20 +1,21 @@
 $(document).ready(function() {
-	// setOrientationListener(); 
-	$('#calendar').calendar({
-		type: 'date'
-	});
+    // setOrientationListener(); 
+    // $('#calendar').calendar({
+    //     type: 'date'
+    // });
+
 });
 
 // function setOrientationListener() {
-// 	rotationInterval = setInterval(function() {
-// 		updateOrientation();
-// 	}, 500);
+//  rotationInterval = setInterval(function() {
+//      updateOrientation();
+//  }, 500);
 // }
 
 // function updateOrientation() {
-// 	if ($('body').width() < 1024) {
-// 		$('.page').removeClass('landscape').addClass('portrait');
-// 	} else {
-// 		$('.page').removeClass('portrait').addClass('landscape');
-// 	}
+//  if ($('body').width() < 1024) {
+//      $('.page').removeClass('landscape').addClass('portrait');
+//  } else {
+//      $('.page').removeClass('portrait').addClass('landscape');
+//  }
 // }
