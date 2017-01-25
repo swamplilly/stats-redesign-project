197013713906b7a45a06bf318fa377b187823664
Lilly An <li.liwei94@gmail.com>
2017-01-25 15:46:31 -0800

    added swipe mockup

--- webapp/assets/includes/custom.swipe.js
+++ webapp/assets/includes/custom.swipe.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
-	$('.item .tab').click(function() {
-		$('.active').removeClass('active');
-		$(this).addClass('active');
+	$('body').click(function() {
+		// $('#ref').toggleClass('hidden');
+		$('#ref').transition({
+			animation : 'fade left',
+			duration : 1000,
+			interval : 200
+		});
+		// $('#qq').toggleClass('hidden');
+		$('#qq').transition({
+			animation : 'fade right',
+			duration : 1000
+		});
 	});
-
-	$('.menu .item').tab();
 });
